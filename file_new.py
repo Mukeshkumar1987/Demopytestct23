b1a9ae3f719a6c7c8cda68ca9cfa3a063f023cdc
MukeshDeshpande <deshpandemukesh35@gmail.com>
2023-05-08 12:28:49 +0530

    first commit

---
+++ file_new.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+class Test_002:
+
+
+    def test_sum_004(self):
+        a = 5
+        b = 4
+        sum = a + b
+        if sum == 9:
+            assert True
+        else:
+            assert False
